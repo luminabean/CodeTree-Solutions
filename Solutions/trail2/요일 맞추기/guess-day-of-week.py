ac08b98d9bfacb18ed92f0b1aa3307116d1c8ff5
빛콩 <38746279+luminabean@users.noreply.github.com>
2026-05-11 14:03:32 +0900

    [Passed] 요일 맞추기 (44ms, 16MB)

---
+++ Solutions/trail2/요일 맞추기/guess-day-of-week.py
@@ -0,0 +1,10 @@
+m1, d1, m2, d2 = map(int, input().split())
+
+# Please write your code here.
+days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+yoil = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+m1_days = sum(days[:m1-1]) + d1
+m2_days = sum(days[:m2-1]) + d2
+var = m2_days - m1_days
+
+print(yoil[var % 7])
