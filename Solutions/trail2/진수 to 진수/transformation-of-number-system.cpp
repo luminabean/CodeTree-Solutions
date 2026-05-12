4ab3b14994c5640a92f9fc5975a98ce97f18fb55
빛콩 <38746279+luminabean@users.noreply.github.com>
2026-05-12 23:07:57 +0900

    [Passed] 진수 to 진수 (8ms, 0MB)

---
+++ Solutions/trail2/진수 to 진수/transformation-of-number-system.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int a, b;
+string n;
+
+// n진수를 10진수로
+int nToDec(int n, string num) {
+    int dec_num = 0;
+
+    for(int i=0; i<num.length(); i++) {
+        dec_num = dec_num*n + int(num[i]-48);
+    }
+    return dec_num;
+}
+
+string decToN(int n, int num) {
+    string n_num = "";
+
+    while(num>=n) {
+        n_num += char((num%n)+48);
+        num /= n;
+    }
+    n_num += char(num+48);
+    reverse(n_num.begin(), n_num.end());
+
+    return n_num;
+}
+
+int main() {
+    cin >> a >> b;
+    cin >> n;
+
+    // Please write your code here.
+    cout << decToN(b, nToDec(a, n));
+    return 0;
+}
