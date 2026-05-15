5cf5f7e84723e1589fbe461b81f3b8747b80a8c5
빛콩 <38746279+luminabean@users.noreply.github.com>
2026-05-15 19:34:43 +0900

    [Passed] 신기한 타일 뒤집기 (9ms, 1MB)

---
+++ Solutions/trail2/신기한 타일 뒤집기/strange-flipping-tiles.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int x[1000];
+char dir[1000];
+
+int main() {
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> x[i] >> dir[i];
+    }
+
+    // Please write your code here.
+    int color[200000] = {0, };  // -1 = White, 0 = None, 1 = Black
+    int cur = 100000;  // 현재 화표
+
+    for (int i = 0; i < n; i++) {
+        // 순서 상관없이 흰색과 검은색으로 각각 두 번 이상 칠해지면 회색으로 바뀜
+        // 규칙 특성상 검은색과 흰색은 번갈아가면서 나옴. 따라서 4번 이상 방문하면 회색으로 바뀐다고 보면 됨
+        if (dir[i] == 'R') {
+            for (int j = 0; j < x[i]; j++) {
+                color[cur] = 1;
+                
+                if (j != x[i] - 1)
+                    cur++;
+            }
+        }
+        else if (dir[i] == 'L') {
+            for (int j = 0; j < x[i]; j++) {
+                color[cur] = -1;
+
+                if (j != x[i] - 1)
+                    cur--;
+
+            }
+        }
+    }
+
+    int black = 0, white = 0, grey = 0;
+    for (int i = 0; i < 200000; i++) {
+        if (color[i] == -1)
+            white++;
+        else if (color[i] == 1)
+            black++;
+    }
+
+    cout << white << " " << black;;
+
+    return 0;
+}
