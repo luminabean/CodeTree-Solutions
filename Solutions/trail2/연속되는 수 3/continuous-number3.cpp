6e4c4bb8328c9ade644ca631b82d22b4bb7000b1
빛콩 <38746279+luminabean@users.noreply.github.com>
2026-05-18 22:01:04 +0900

    [Passed] 연속되는 수 3 (8ms, 0MB)

---
+++ Solutions/trail2/연속되는 수 3/continuous-number3.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+int arr[1000];
+
+int main() {
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+
+    // Please write your code here.
+    int ans = 0, streak = 1;
+    for (int i = 1; i < N; i++) {
+        if (arr[i-1] * arr[i] < 0) {
+            if (ans < streak)
+                ans = streak;
+            streak = 1;
+        }
+        else
+            streak++;
+    }
+    ans = ans < streak ? streak : ans;
+    cout << ans;
+    return 0;
+}
