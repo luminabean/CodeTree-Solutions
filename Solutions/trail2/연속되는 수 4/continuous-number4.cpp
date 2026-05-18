eed44111fc1cc1ca3a337c3ac07abdb6410b83f3
빛콩 <38746279+luminabean@users.noreply.github.com>
2026-05-18 22:07:33 +0900

    [Passed] 연속되는 수 4 (8ms, 0MB)

---
+++ Solutions/trail2/연속되는 수 4/continuous-number4.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+int arr[1000];
+
+int main() {
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+
+    // Please write your code here.
+    int ans = 1, streak = 1;
+    for (int i = 1; i < N; i++) {
+        if (arr[i-1] >= arr[i]) {
+            streak = 1;
+        }
+        else
+            streak++;
+
+        ans = ans < streak ? streak : ans;
+    }
+    cout << ans;
+    return 0;
+}
