2c8cc73160a229268614e50f5d8239f51bdbfd2b
빛콩 <38746279+luminabean@users.noreply.github.com>
2026-05-25 17:32:51 +0900

    [Passed] 별 찍는 것을 5번 반복하기 (2ms, 0MB)

---
+++ Solutions/trail2/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void makeStar() {
+    cout << "**********" << endl;
+}
+
+int main() {
+    // Please write your code here.
+    for (int i = 0; i < 5; i++)
+        makeStar();
+    return 0;
+}
