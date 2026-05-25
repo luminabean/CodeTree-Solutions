d98af99f57a666b87bb4bf5517e8b7b9519ed582
빛콩 <38746279+luminabean@users.noreply.github.com>
2026-05-25 17:34:17 +0900

    [Passed] 반복 출력하기 (2ms, 0MB)

---
+++ Solutions/trail2/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+void print(int N) {
+    while(N--)
+        cout << "12345^&*()_" << endl;
+}
+
+int main() {
+    cin >> N;
+
+    // Please write your code here.
+    print(N);
+    return 0;
+}
